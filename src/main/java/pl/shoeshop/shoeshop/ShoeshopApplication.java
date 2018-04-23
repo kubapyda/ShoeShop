8c3e091398f04def9092132dd38735d4b63677ff
Wojciech <wojciech.niemiec.pv@gmail.com>
2018-04-23 23:24:10 +0200

    Configured application for deployment on AWS

--- src/main/java/pl/shoeshop/shoeshop/ShoeshopApplication.java
+++ src/main/java/pl/shoeshop/shoeshop/ShoeshopApplication.java
@@ -3,10 +3,17 @@ package pl.shoeshop.shoeshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @LiquibaseDataSource
-public class ShoeshopApplication {
+public class ShoeshopApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ShoeshopApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoeshopApplication.class, args);
